ffee9ab931b9607100d89edfe129cba1f119b5c4
Josh <paystey2k5@gmail.com>
2011-05-01 07:51:41 +0100

    Initial Commit

---
+++ __init__.py
@@ -0,0 +1,24 @@
+from twisted.internet import reactor
+from pymail.database import database
+from pyMail.smtp import protocol, queue, delivery
+from config import settings 
+import cPickle
+
+db = database.instance()
+
+if ( settings['services']['smtp']['on'] ):    
+    print 'Starting SMTP on port %s' % (settings['services']['smtp']['port'])
+    
+    deliv = delivery.deliveryAgent(db.messages)
+    queue = queue.queue(db.queue, deliv)    
+    queue.restore()   
+    
+    reactor.listenTCP(settings['services']['smtp']['port'], protocol.factory(settings['services']['smtp'], queue))
+    
+if ( settings['services']['smtps']['on'] ):    
+    reactor.listenSSL(settings['services']['smtps']['port'], protocol.factory(settings['services']['smtps'], queue))
+
+if ( settings['services']['imap']['on'] ):    
+    reactor.listenTCP(settings['services']['imap']['port'], protocol.factory(settings['services']['imap'], queue))
+    
+reactor.run()
