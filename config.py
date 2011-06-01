095ef4193f37527daa7602177b97499b129656a1
Josh <paystey2k5@gmail.com>
2011-06-01 23:37:37 +0100

    Implemented config class for fancy config stuff (planned runtime updates to config so no restart)
    Also begun implementation of configuration for domains that the server handles.

--- config.py
+++ config.py
@@ -1,42 +1,26 @@
-settings = {
-    'services':
-        {
-            'smtp':
-                {
-                    'on': True,
-                    'welcome': 'Hai Guyz!',
-                    'port': 25, #default = 25
-                    'events':{
-                        'beforeDelivery':[],
-                        'afterDelivery':[],
-                        'beforeLocalDelivery':[],
-                        'afterLocalDelivery':[],
-                        'beforeRemoteDelivery':[],
-                        'afterRemoteDelivery':[],
-                        'onHelo':[],
-                        'afterHelo':[],
-                        'onMail':[],
-                        'afterMail':[],
-                        'onRcpt':[],
-                        'afterRcpt':[],
-                    }
-                },
-            'smtps':
-                {
-                    'on': False,
-                    'port': 867
-                },
-            'imap':
-                {
-                    'on': False,
-                    'port': 143
-                },
-        },
-    'db':
-        {
-            'module': 'pyMongo',
-            'dsn': '',
-            'user': '',
-            'pass': '',
-        }
-}
+from settings import settings
+class config:
+	def __init__(self, db):
+		self.settings = settings
+		self.db = db
+		self.populateDomains(True)
+		
+	def populateDomains(self, force=False):
+		""" Parse domains from DB for speeds sake e.g. 'dev.co.uk' in settings['domains'] 
+		 even though its an alias and therefore nested in the DB """
+		if not 'domains' in self.settings or force:
+			domains = self.db.domains.find()
+			
+			self.settings['domains'] = {}
+			for dom in domains:
+				self.settings['domains'][dom['name']] = {
+					'parent': dom['name'],	
+				}
+				for alias in dom['alias']:
+					self.settings['domains'][alias] = {
+						'parent':dom['name']
+					}
+		return self.settings['domains']
+
+	def __getitem__(self, var):
+		return self.settings[var]
